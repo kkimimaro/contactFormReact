0ae55e321295d4ccf4bf20782472eeb530a1cb14
kkimimaro <dannu2001@gmail.com>
2021-05-12 18:03:42 +0300

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        axios({
+            method: "POST",
+            url:"http://127.0.0.1/www.rustamb.ru/index.php",
+            data:  this.state
+        }).then((response)=>{
+            console.log(response)
+            if (response.data.status === 'success') {
+                alert("Ваше сообщение отправлено!");
+                this.resetForm()
+            } else if (response.data.status === 'fail') {
+                alert("Возникла проблема с отправкой")
+            }
+        })
+    }
+
+    resetForm(){
+        this.setState({name: '', email: '', message: ''})
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <h2>Форма обратной связи</h2>
+                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <div className="form-group">
+                        <label htmlFor="name">Имя</label>
+                        <input type="text" className="form-control" placeholder="Введите имя..." required id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email-адрес</label>
+                        <input type="email" className="form-control" placeholder="Введите email..." required id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Сообщение</label>
+                        <textarea className="form-control" rows="5" placeholder="Введите сообщение..." required id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+                    </div>
+                    <input type="submit" className="btn btn-primary" value="ОТПРАВИТЬ"/>
+                </form>
+            </div>
+        );
+    }
+
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+
+    onMessageChange(event) {
+        this.setState({message: event.target.value})
+    }
 }
 
 export default App;
